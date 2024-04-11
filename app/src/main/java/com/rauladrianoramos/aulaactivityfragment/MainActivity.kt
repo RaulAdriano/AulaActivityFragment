8f52566a3a21e27a6459d48e8bafe2a519218c05
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-11 18:50:04 -0300

    navegar entre activitys

--- app/src/main/java/com/rauladrianoramos/aulaactivityfragment/MainActivity.kt
+++ app/src/main/java/com/rauladrianoramos/aulaactivityfragment/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rauladrianoramos.aulaactivityfragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonAbrir: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAbrir = findViewById(R.id.button_abrir)
+        buttonAbrir.setOnClickListener{
+
+            val intent = Intent(this, DetalhesActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
     }
 }
