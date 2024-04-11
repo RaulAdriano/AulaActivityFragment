b218b992659e01d2b7c3605fc7426eea902af3b2
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-11 08:58:24 -0300

    criacao activity nova

--- app/src/main/java/com/rauladrianoramos/aulaactivityfragment/DetalhesActivity.kt
+++ app/src/main/java/com/rauladrianoramos/aulaactivityfragment/DetalhesActivity.kt
@@ -3,9 +3,9 @@ package com.rauladrianoramos.aulaactivityfragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DetalhesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detalhes)
     }
 }
